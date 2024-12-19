943c7f4223b64d1f3165cd0df630606687faedca
Sven Lüpke <luepke@in.tum.de>
2024-12-19 14:02:42 +0100

    Update script

--- codebase/run_unobserved_experiments.sh
+++ codebase/run_unobserved_experiments.sh
@@ -7,6 +7,7 @@
 #SBATCH --gres=gpu:1                    # Number of GPUs (if needed)
 
 # Run your commands
+pip list
 python -m data.generate_dataset
 python -m train --suffix _springs5  # observed
 python -m train --suffix _springs5 --unobserved 1 --model_unobserved 0  # ACD with latent
