bf39e1761693fb2a31c0c70c52bb64b9a79bce20
Sven Lüpke <luepke@in.tum.de>
2024-12-19 13:50:24 +0100

    Update script

--- codebase/run_unobserved_experiments.sh
+++ codebase/run_unobserved_experiments.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#SBATCH --output=output_%j.log          # Standard output and error log (%j will be replaced by job ID)
+#SBATCH --error=error_%j.log            # Error log
+#SBATCH --partition=mcml-hgx-a100-80x4  # Partition name
+#SBATCH --ntasks=1                      # Number of tasks
+#SBATCH --qos=mcml                      # Quality of service
+#SBATCH --gres=gpu:1                    # Number of GPUs (if needed)
+
+# Run your commands
 python -m data.generate_dataset
 python -m train --suffix _springs5  # observed
 python -m train --suffix _springs5 --unobserved 1 --model_unobserved 0  # ACD with latent
 python -m train --suffix _springs5 --unobserved 1 --model_unobserved 1  # None
-python -m train --suffix _springs5 --unobserved 1 --model_unobserved 2  # Mean
