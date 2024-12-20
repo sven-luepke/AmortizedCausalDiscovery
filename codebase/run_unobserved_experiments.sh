c1b1826c298f6b7c8f6529a0bc253407e68753fe
Sven Lüpke <luepke@in.tum.de>
2024-12-20 12:43:29 +0100

    Update confounder experiment

--- codebase/run_unobserved_experiments.sh
+++ codebase/run_unobserved_experiments.sh
@@ -14,15 +14,15 @@ cd ~/workspace/AmortizedCausalDiscovery/codebase
 pwd
 
 # Table 4
-python -u -m data.generate_dataset
-python -u -m train --suffix _springs5 # observed
-python -u -m train --suffix _springs5 --unobserved 1 --model_unobserved 0  # ACD with latent
-python -u -m train --suffix _springs5 --unobserved 1 --model_unobserved 1  # None
-python -u -m train --suffix _springs5 --unobserved 1 --model_unobserved 2  # Mean
+python -u -m data.generate_dataset --confounder
+python -u -m train --suffix _springs5_conf # observed
+python -u -m train --suffix _springs5_conf --unobserved 1 --model_unobserved 0 --dont_shuffle_unobserved  # ACD with latent
+python -u -m train --suffix _springs5_conf --unobserved 1 --model_unobserved 1 --dont_shuffle_unobserved  # None
+python -u -m train --suffix _springs5_conf --unobserved 1 --model_unobserved 2 --dont_shuffle_unobserved  # Mean
 
 # Table 5
 python -u -m data.generate_dataset --uninfluenced_particle --influencer_particle
 python -u -m train --suffix _springs5_uninfluenced_influencer  # observed
-python -u -m train --suffix _springs5_uninfluenced_influencer --unobserved 1 --model_unobserved 0  # ACD with latent
-python -u -m train --suffix _springs5_uninfluenced_influencer --unobserved 1 --model_unobserved 1  # None
-python -u -m train --suffix _springs5_uninfluenced_influencer --unobserved 1 --model_unobserved 2  # Mean
+python -u -m train --suffix _springs5_uninfluenced_influencer --unobserved 1 --model_unobserved 0 --dont_shuffle_unobserved  # ACD with latent
+python -u -m train --suffix _springs5_uninfluenced_influencer --unobserved 1 --model_unobserved 1 --dont_shuffle_unobserved  # None
+python -u -m train --suffix _springs5_uninfluenced_influencer --unobserved 1 --model_unobserved 2 --dont_shuffle_unobserved  # Mean
