7f5acea08eaa4078142088d3c2570a5dc4aa9e82
Sven Lüpke <luepke@in.tum.de>
2025-01-04 17:29:31 +0100

    Update slurm partition

--- codebase/run_unobserved_new.sh
+++ codebase/run_unobserved_new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --output=output_%j.log          # Standard output and error log (%j will be replaced by job ID)
 #SBATCH --error=error_%j.log            # Error log
-#SBATCH --partition=mcml-hgx-a100-80x4  # Partition name
+#SBATCH --partition=mcml-dgx-a100-40x8  # Partition name
 #SBATCH --ntasks=4                      # Number of tasks
 #SBATCH --qos=mcml                      # Quality of service
 #SBATCH --gres=gpu:1                    # Number of GPUs (if needed)
