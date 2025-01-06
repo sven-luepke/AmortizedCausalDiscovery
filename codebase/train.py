af1ad36fca79c04d614b4f7bc01aa2fbe3f3f416
Sven Lüpke <luepke@in.tum.de>
2025-01-06 10:52:24 +0100

    Update experiment script

--- codebase/train.py
+++ codebase/train.py
@@ -165,7 +165,7 @@ def test(encoder, decoder, epoch):
                 temperatures=temperatures,
             )
 
-            for j in range(1):
+            for j in range(4):
                 ############ Plotting ############
                 import os
                 import matplotlib.pyplot as plt
