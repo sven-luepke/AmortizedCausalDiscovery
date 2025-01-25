8f709de30e454e03a8bb9671d2bceb8d579616a2
Sven Lüpke <luepke@in.tum.de>
2025-01-25 18:00:09 +0100

    Add A100 and H100 experiment scripts

--- codebase/run_unobserved_cmp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# old model - new model v1 - new model v2
-python -u -m train --suffix _springs5 --dont_shuffle_unobserved --epochs=8 --unobserved 1 --model_unobserved 3 --predict_initial_point
-python -u -m train --suffix _springs5 --dont_shuffle_unobserved --epochs=8 --unobserved 1 --model_unobserved 3
-python -u -m train --suffix _springs5 --dont_shuffle_unobserved --epochs=8 --unobserved 1 --model_unobserved 0
