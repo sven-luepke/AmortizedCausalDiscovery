d5d3de7240fa421e25d2c490b87d9eb974a22b9e
Sven Lüpke <luepke@in.tum.de>
2025-04-26 19:46:32 +0200

    Add data and train scripts

---
+++ codebase/train_dynamic_1_springs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --output=output_%j.log          # Standard output and error log (%j will be replaced by job ID)
+#SBATCH --error=error_%j.log            # Error log
+#SBATCH --partition=mcml-hgx-a100-80x4  # Partition name
+#SBATCH --ntasks=2                      # Number of tasks
+#SBATCH --qos=mcml                      # Quality of service
+#SBATCH --gres=gpu:1                    # Number of GPUs (if needed)
+#SBATCH --container-image="nvcr.io/nvidia/pytorch:24.11-py3"
+#SBATCH -D ./
+#SBATCH -t 2-00:00:00
+
+cd ~/workspace/AmortizedCausalDiscovery/codebase
+pwd
+
+python -m train --suffix _springs5_dynamic1 --epochs=500 --encoder=transformer
